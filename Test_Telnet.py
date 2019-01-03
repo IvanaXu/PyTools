903065c676fa3d8d7b9b77a5a43d6a17304d945a
ivan <ivan@yiyin.site>
2019-01-03 16:05:08 +0800

    Test Telnet

---
+++ Test_Telnet.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+# @auth ivan
+# @time 20190101
+# @goal test the telnet
+import telnetlib
+import time
+import random
+
+h, p, n = "0.0.0.0", 11111, 0
+while True:
+    time.sleep(random.randrange(0, 1000)/1000)
+    n += 1
+    try:
+        tb = telnetlib.Telnet()
+        tb.open(host=h, port=p, timeout=1)
+        print("Step %5d" %n, tb.read_very_eager().decode(), "-"*20)
+        tb.close()
+    except Exception as e:
+        print("Fail %5d" % n, e)
+
+
+
