b4839c0b9e3b29dff76949612a8287592c41cded
ivan <ivan@yiyin.site>
2019-01-03 16:15:45 +0800

    Test os.walk

---
+++ Test_CleanBigFile.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+# @auth ivan
+# @time 2018-07-16 11:45:41
+# @goal clean the big file
+
+import os
+import pandas as pd
+root_list = ['/data/project/Temp']
+result = []
+
+print("Start!")
+for root in root_list:
+    for i, j, k in os.walk(root):
+        for t in k:
+            p = i + os.path.sep + t
+            print(p)
+            data = pd.DataFrame([[p, os.path.getsize(p)]], columns=['File', 'Size'])
+            result.append(data)
+print("Finis!")
+data_all = pd.DataFrame(pd.concat(result))
+data_all.sort_values('Size', inplace=True)
+print(data_all)
+
+
+
