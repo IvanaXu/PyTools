f72732ed328b37fbeb47963af89b62dcf2f43d4b
ivan <1440420407@qq.com>
2019-10-20 19:07:23 +0800

    UPDATE 026.Py2Pyc2Pyo_Test c /

---
+++ 026.Py2Pyc2Pyo/setup.sh
@@ -0,0 +1,2 @@
+py3 setup.py build_ext
+cp build/lib.linux-x86_64-3.6/PTest026.cpython-36m-x86_64-linux-gnu.so PTest026.so 
--- Temp/PTest026.py
+++
@@ -1,3 +0,0 @@
-class A(object):
-    def __init__(self):
-        print(hex(int(1.2e9)+10086))
