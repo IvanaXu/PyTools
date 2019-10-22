4d1fe77f2bc785302490d3c6b3fbc2376a4a5fb8
ivan <1440420407@qq.com>
2019-10-22 11:59:32 +0800

    update 032.Test_Mrjob_MapReduce //file

---
+++ 032.Test_Mrjob_MapReduce/run.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from mrjob.job import MRJob
+
+class MRWordCounter(MRJob):
+    def mapper(self,key,line): 
+        for word in line.split():
+            yield word,1
+
+    def reducer(self,word,occurrences): 
+        yield word,sum(occurrences)
+
+if __name__ == '__main__':
+    MRWordCounter.run()
