8b3b2f85bb44feeb88ab3565326ddb5d7cf09e6d
ivan <1440420407@qq.com>
2020-05-18 23:21:08 +0800

    update code 20200518232108

---
+++ 036.PyCrawler_Spiget/036.Test_Demo_N00.py
@@ -0,0 +1,15 @@
+# 036.Test_Demo_N00.py
+import re
+import requests
+cont = requests.get("https://m.maigoo.com/news/483761.html").content.decode("utf-8")
+pattern = re.compile(r'[\u4e00-\u9fa5]+')
+for i in cont.split("\n"):
+    s1 = """html" title="""
+    if s1 in i and pattern.search(i):
+        print(i[i.find(s1)+len(s1)+1:-2])
+
+    s21 = """html" >"""
+    s22 = """</a></td>"""
+    if s21 in i and s22 in i and pattern.search(i):
+        print(i[i.find(s21)+len(s21):-len(s22)])
+
---
+++ 036.PyCrawler_Spiget/036.Test_Demo_N01.py
@@ -0,0 +1,9 @@
+# 036.Test_Demo_N01.py
+import re
+import requests
+cont = requests.get("https://m.maigoo.com/news/485519.html").content.decode("utf-8")
+pattern = re.compile(r'[\u4e00-\u9fa5]+')
+for i in cont.split("\n"):
+    s1 = """<td class="md_td">"""
+    if s1 in i and pattern.search(i):
+        print(i[i.find(s1)+len(s1):-5])
