211fd1a9c81f4c1619e0c0310aa769bd8a6db299
ivan <1440420407@qq.com>
2020-06-08 18:08:06 +0800

    update code 20200608180806

--- 053.Test_DiffMatch.py
+++ 053.Test_DiffMatch.py
@@ -10,7 +10,7 @@ s2 = "Test the python"
 for _ in difflib.SequenceMatcher(None, s1, s2).get_matching_blocks():
     if _.size > 3:
         print(s1[_.a: _.a+_.size])
-# Test the
+# Test the XXX
 
 
 
