9604778dbbdd1a6cdc8a647babf0489b63235dc9
ivan <1440420407@qq.com>
2020-06-15 07:31:55 +0800

    update code 20200615073155

---
+++ 056.Test_MyQR.py
@@ -0,0 +1,14 @@
+#
+from MyQR import myqr
+myqr.run(
+    words='http://www.baidu.com',
+    save_name='056.Test_MyQR.png'
+)
+
+# import zxing
+# reader = zxing.BarCodeReader()
+# barcode = reader.decode("056.Test_MyQR.png")
+# print(barcode.parsed)
+
+
+
