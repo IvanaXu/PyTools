dbd8eb4e12232bf48239ead1bcf7f20100aae0a3
ivan <1440420407@qq.com>
2020-06-20 12:25:42 +0800

    update code 20200620122541

--- 057.Test_wget.py
+++ 057.Test_wget.py
@@ -1,3 +1,12 @@
+# -*-coding:utf-8-*-
+# @auth ivan
+# @time 20200620 12:24:19 
+# @goal wget the file online
+"""
+SOMETIME U USE
+    os.system("wget ... -o ...")
+WILL BE STOP.
+"""
 import wget
 DATA_URL = 'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png' 
 DATA_PATH = '057.Test_wget.png'
