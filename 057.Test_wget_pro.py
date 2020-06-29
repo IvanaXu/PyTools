b6a6fae39f3f044a5f67f19986ee93f8234395ac
ivan <1440420407@qq.com>
2020-06-29 09:58:25 +0800

    update code 20200629095825

--- 057.Test_wget_pro.py
+++ 057.Test_wget_pro.py
@@ -7,16 +7,10 @@ with open("train_annotations.json") as f:
 with open("val_video_ids.txt") as f:
     lvals = [k.strip("\n") for k in f]
 
-N = 5
-title = "http://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/531798"
+N, title = 5, "http://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/531798"
 
 def getit(lid, subset, subtyp, subout):
-    """
-    :param lid: THE ID LIST.
-    :param subset: train, val
-    :param subtyp: video, i3d_feature, vggish_feature
-    :param subout: THE OUTPUT PATH.
-    """
+    """@lid:ID;@subset:train,val;@subtyp:video,i3d_feature,vggish_feature;@subout:OUTPUT"""
     n = 0
     for kid in lid:
         url = f"{title}/{subset}/{subtyp}/{kid}.npy"
@@ -24,8 +18,7 @@ def getit(lid, subset, subtyp, subout):
             try:
                 wget.download(url, out=f"{subout}/{subtyp}_{kid}.npy")
             except Exception as e:
-                print(n, k, e)
+                print(n, kid, e)
         n += 1
-
 getit(ltrai, "train", "vggish_feature", "outs/trai")
 getit(lvals, "val", "vggish_feature", "outs/vals")
