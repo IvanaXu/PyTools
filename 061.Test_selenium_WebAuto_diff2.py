fb52bb6bd44ed59fb67a9d33fc5006e2685e6fcd
ivan <1440420407@qq.com>
2020-08-04 10:29:47 +0800

    update code 20200804102947

---
+++ 061.Test_selenium_WebAuto_diff2.py
@@ -0,0 +1,29 @@
+#
+from selenium import webdriver
+
+url = "https://www.baidu.com"
+
+user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.3 Safari/605.1.15"
+
+# 伪装user_agent 并进一步反爬虫
+opt = webdriver.ChromeOptions()
+opt.add_argument('--user-agent=%s' % user_agent)
+opt.add_experimental_option('excludeSwitches', ['enable-automation'])
+opt.add_experimental_option('useAutomationExtension', False)
+
+# http://npm.taobao.org/mirrors/chromedriver/
+browser = webdriver.Chrome(executable_path="./061.Test_selenium_WebAuto_chromedriver", options=opt)
+browser.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+  "source": """
+    Object.defineProperty(navigator, 'webdriver', {
+      get: () => undefined
+    })
+  """
+})
+
+browser.get(url)
+
+print(browser.page_source)
+
+
+
---
+++ 061.Test_selenium_WebAuto_diff3.py
@@ -0,0 +1,36 @@
+#
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+
+url = "https://www.baidu.com"
+
+user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.3 Safari/605.1.15"
+
+# 伪装user_agent 并进一步反爬虫
+opt = webdriver.ChromeOptions()
+opt.add_argument('--user-agent=%s' % user_agent)
+opt.add_experimental_option('excludeSwitches', ['enable-automation'])
+opt.add_experimental_option('useAutomationExtension', False)
+
+# http://npm.taobao.org/mirrors/chromedriver/
+browser = webdriver.Chrome(executable_path="./061.Test_selenium_WebAuto_chromedriver", options=opt)
+browser.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+  "source": """
+    Object.defineProperty(navigator, 'webdriver', {
+      get: () => undefined
+    })
+  """
+})
+wait = WebDriverWait(browser, 30)
+browser.get(url)
+# F11 Copy Xpath
+browser.find_element_by_xpath("""//*[@id="kw"]""").send_keys("10086")
+
+# F11 Copy JSpath
+browser.execute_script("""document.querySelector("#su").click()""")
+
+print(browser.page_source)
+
+
+
+# 036.PyCrawler_Spiget
