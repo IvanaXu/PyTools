fe9382af5437b0bce516519e775d103d9f0f7363
ivan <1440420407@qq.com>
2020-10-12 17:26:05 +0800

    update code 20201012172605

--- 070.YAPF_PEP8_3.py
+++ 070.YAPF_PEP8_3.py
@@ -16,4 +16,24 @@ class foo  (     object  ):
 def f  (   a ) :
   return      37+-+a[42-x :  y**3]
 """
-print(FormatCode(code))
+print(FormatCode(code)[0])
+"""
+x = {'a': 37, 'b': 42, 'c': 927}
+
+y = 'hello ' 'world'
+z = 'hello ' + 'world'
+a = 'hello {}'.format('world')
+
+
+class foo(object):
+    def f(self):
+        return 37 * -+2
+
+    def g(self, x, y=42):
+        return y
+
+
+def f(a):
+    return 37 + -+a[42 - x:y**3]
+
+"""
