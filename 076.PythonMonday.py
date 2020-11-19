a45687021f703080d55ffc21113b23f74b217892
ivan <1440420407@qq.com>
2020-11-19 15:14:37 +0800

    update code 20201119151437

---
+++ 076.PythonMonday.py
@@ -0,0 +1,85 @@
+# http://damiantgordon.com/PythonMonday/
+
+# Page3
+print("-" * 100)
+print("Hello, World!")
+
+# Page4
+print("-" * 100)
+print("Hello, World!")
+print("Hello, World!\n")
+print("\nHello, World!")
+print("Hello, \nWorld!")
+print("Hello,")
+print("World!")
+
+# Page 5
+print("-" * 100)
+print("Hello," + " World!")
+print("19" + "91")
+print("Hello, World!" * 3)
+print("Hello, World! " * 3)
+print("Hello, World!\n" * 3)
+print("9" * 3)
+
+# Page 6
+print("-" * 100)
+print(19 + 91)
+print(19 - 91)
+print(19 * 91)
+print(19 / 91)
+
+# Page 7
+print("-" * 100)
+print(11 / 4)
+print(11 // 4)
+print(11 % 4)
+#
+print(11 // 1)
+print(11 // 2)
+print(11 // 3)
+print(11 // 4)
+print(11 // 5)
+print(11 // 6)
+#
+print(11 % 1)
+print(11 % 2)
+print(11 % 3)
+print(11 % 4)
+print(11 % 5)
+print(11 % 6)
+
+# Page 9
+print("-" * 100)
+X = 5
+print(X)
+print("X")
+print(X + 1)
+print(X)
+print(X + 10)
+
+# Page 10
+print("-" * 100)
+X = 5
+Y = 15
+print(X)
+print(Y)
+
+X = 5
+Y = X + 10
+print(Y)
+
+print(X + Y)
+print(X, Y)
+
+# Page 12
+print("-" * 100)
+X = False
+print(X)
+
+X = True
+Y = not (X)
+print(Y)
+
+Z = not (not (X))
+print(Z)
