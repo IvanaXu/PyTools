8d39b894437d1a86185587869758d3ca8ae8a87a
ivan <1440420407@qq.com>
2020-11-19 17:42:39 +0800

    update code 20201119174239

--- 077.Test_BeeWare.sh
+++ 077.Test_BeeWare.sh
@@ -1,7 +1,21 @@
+#
 python -m pip install briefcase -i https://pypi.tuna.tsinghua.edu.cn/simple
 
+#
 briefcase new
 
+#
 cd helloworld
 briefcase dev
 
+#
+briefcase create
+
+#
+briefcase build
+
+#
+briefcase run
+
+#
+briefcase package --no-sign
