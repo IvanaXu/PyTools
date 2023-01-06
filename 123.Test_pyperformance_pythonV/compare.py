c24c20cb03e22bd6d099562282ffe8116fa46a9e
IvanaXu <1440420407@qq.com>
2023-01-06 16:56:52 +0800

    update 123.Test_pyperformance_pythonV

---
+++ 123.Test_pyperformance_pythonV/compare.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+data = []
+for t in ["3.9.0", "3.10.0", "3.11.0", "3.11.1"]:
+
+    t1, t2, tdata = "", "", {}
+    with open(f"{t}/py{t}.log") as f:
+        for i in f:
+            i = i.strip("\n")
+            
+            if "###" in i:
+                t1 = i
+            if "Mean" in i:
+                t2 = i.replace("Mean +- std dev: ", "")
+                tdata[t1] = t2
+
+                t1, t2 = "", ""
+
+    tdata = pd.DataFrame(
+        [[_k, _v] for _k, _v in tdata.items()],
+        columns=["Task", t]
+    )
+    data.append(tdata)
+
+sdata = pd.DataFrame()
+for tdata in data:
+    if len(sdata) == 0:
+        sdata = tdata
+    else:
+        sdata = pd.merge(sdata, tdata, on="Task", how="outer")
+
+with open("compare.md", "w") as f:
+    for i in sdata.fillna("/").to_markdown():
+        f.write(i)
