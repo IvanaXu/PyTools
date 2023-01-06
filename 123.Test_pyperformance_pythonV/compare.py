8fb37d63463014603962fd2376fba984071cc774
IvanaXu <1440420407@qq.com>
2023-01-06 21:52:09 +0800

    Update compare.md compare.py

--- 123.Test_pyperformance_pythonV/compare.py
+++ 123.Test_pyperformance_pythonV/compare.py
@@ -2,10 +2,11 @@ import os
 import pandas as pd
 
 data = []
-for t in sorted(
+TASK = sorted(
     [i for i in os.listdir() if "3." in i],
     key=lambda x: float(x.replace("3.", ""))
-):
+)
+for t in TASK:
 
     t1, t2, tdata = "", "", {}
     with open(f"{t}/py{t}.log") as f:
@@ -33,6 +34,42 @@ for tdata in data:
     else:
         sdata = pd.merge(sdata, tdata, on="Task", how="outer")
 
+
+def fchange(string):
+    if pd.isna(string):
+        return string
+    string = str(string).split("+-")[0]
+
+    result = -999999
+    if "sec" in string:
+        result = float(string.replace("sec", ""))*1000
+    elif "ms" in string:
+        result = float(string.replace("ms", ""))
+    elif "us" in string:
+        result = float(string.replace("us", ""))/1000
+    elif "ns" in string:
+        result = float(string.replace("ns", ""))/1000/1000
+    return round(result, 4)
+
+def change(df):
+    _df = df.copy()
+    for icol in TASK:
+        _df[icol] = _df[icol].apply(fchange)
+    return _df
+
+cdata = change(sdata)
+cdata0 = cdata.copy()
+cdata.loc["SUM"] = [""] + [round(cdata0[icol].sum(), 4) for icol in  TASK]
+cdata.loc["AVG"] = [""] + [round(cdata0[icol].mean(), 4) for icol in  TASK]
+
 with open("compare.md", "w") as f:
+    f.write("### 0.Source\n")
     for i in sdata.fillna("/").to_markdown():
         f.write(i)
+    f.write("\n\n")
+
+    f.write("### 1.Change\n")
+    for i in cdata.fillna("/").to_markdown():
+        f.write(i)
+    f.write("\n\n")
+    
